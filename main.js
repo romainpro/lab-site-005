fe8f45cb257595a191d3c4f0c304f17c2908e914
romainpro <romain.casagrande.pro@gmail.com>
2023-05-19 21:33:57 +0200

    add-li*4 with loop for

--- main.js
+++ main.js
@@ -7,6 +7,12 @@ let mainBar = document.getElementById("mainNav")
 function createNavBar (){
     let ul = document.createElement("ul");
     mainBar.appendChild(ul);
+        for (let i = 0; i < 4 ; i++){
+            let li = document.createElement("li");
+            ul.appendChild(li);
+            // console.log(i);
+        }
+    
 
 }
 
