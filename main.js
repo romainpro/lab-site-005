c09de5624f64801de61ce99ffffbc1b66d3b55b9
romainpro <romain.casagrande.pro@gmail.com>
2023-05-19 21:45:21 +0200

    add link href

--- main.js
+++ main.js
@@ -9,7 +9,11 @@ function createNavBar (){
     mainBar.appendChild(ul);
         for (let i = 0; i < 4 ; i++){
             let li = document.createElement("li");
+            let a= document.createElement("a");
+            a.href="#"
+            a.textContent="page "+(i+1);
             ul.appendChild(li);
+            li.appendChild(a);
             // console.log(i);
         }
     
