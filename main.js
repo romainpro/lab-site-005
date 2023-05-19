197ece8931d120b2e3d392b4d602c65c4a92ac1a
romainpro <romain.casagrande.pro@gmail.com>
2023-05-19 21:17:56 +0200

    add navbar html and js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+let mainBar = document.getElementById("mainNav")
 
+
+
+
+
+function createNavBar (){
+    let ul = document.createElement("ul");
+    mainBar.appendChild(ul);
+
+}
+
+createNavBar()
