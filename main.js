0d564c5ddae1415c1e966084eac7a2c932462244
romainpro <romain.casagrande.pro@gmail.com>
2023-05-20 10:20:10 +0200

    fix-const and add href link  with function

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let mainBar = document.getElementById("mainNav")
-
+const iterationLink = 4;  /* const use for function  */
 
 
 
@@ -7,10 +7,10 @@ let mainBar = document.getElementById("mainNav")
 function createNavBar (){
     let ul = document.createElement("ul");
     mainBar.appendChild(ul);
-        for (let i = 0; i < 4 ; i++){
+        for (let i = 0; i < iterationLink ; i++){
             let li = document.createElement("li");
             let a= document.createElement("a");
-            a.href="#"
+            a.href="#page"+(i+1)+".htlm"
             a.textContent="page "+(i+1);
             ul.appendChild(li);
             li.appendChild(a);
